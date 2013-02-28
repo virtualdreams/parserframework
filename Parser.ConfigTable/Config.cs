2099f92a477c27bb13d7da7269a18dd79b26660c
Thomas Kindler <thomas.kindler@lit.justiz.sachsen.de>
2013-02-28 13:10:16 +0100

    - removed test project

--- Parser.ConfigTable/Config.cs
+++ Parser.ConfigTable/Config.cs
@@ -96,6 +96,9 @@ namespace Parser.ConfigTable
 					
 				if(type == typeof(long))
 					return ConfigTypes.Long;
+
+				if (type == typeof(bool))
+					return ConfigTypes.Bool;
 					
 				if(type.IsArray && type == typeof(string[]))
 					return ConfigTypes.Object;
@@ -262,6 +265,35 @@ namespace Parser.ConfigTable
 			{
 				return sr.ReadToEnd();
 			}
+		}
+
+		public void Print()
+		{
+			PegTree tree = _tree;
+			
+			if (tree.Root != null)
+			{
+				int level = 0;
+				PrintNode(tree.Root, level, _source);
+			}
+		}
+
+		private void PrintNode(PegNode node, int level, string source)
+		{
+			string tab = "";
+			for (int i = 0; i < level; ++i)
+				tab += ' ';
+
+			if (node.Id >= 7 && node.Id <= 10 || node.Id == 2)
+				Console.WriteLine("{0}{1} -> {2}", tab, (ConfigTableEnum)node.Id, node.Match.GetString(source));
+			else
+				Console.WriteLine("{0}{1}", tab, (ConfigTableEnum)node.Id);
+
+			if (node.Child != null)
+				PrintNode(node.Child, level + 1, source);
+
+			if (node.Next != null)
+				PrintNode(node.Next, level, source);
 		}
 	}
 }
